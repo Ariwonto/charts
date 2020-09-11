9ecdc2e4413d9c831169039af53d1eea3297913a
Carlos Tovar <carlos.tovar@ariwonto.com>
2020-09-11 17:03:12 -0500

    Update

--- graficos/settings.py
+++ graficos/settings.py
@@ -17,6 +17,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'https://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://127.0.0.1:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
